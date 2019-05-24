1f6dd87d76d0a5c084e3c82c05271012bc69c0db
mroyski <mroyski@gmail.com>
2019-05-24 06:19:26 -0400

    super close

--- HRJumpingOnClouds/Program.cs
+++ HRJumpingOnClouds/Program.cs
@@ -10,21 +10,34 @@ namespace HRJumpingOnClouds
     {
         static void Main(string[] args)
         {
-            int[] clouds = new int[] { 0, 0, 1, 0, 0, 1, 0, };
-            int n = clouds.Length - 1;
+            int[] c = new int[] { 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0,
+                1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0,
+                0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1,
+                0, 0, 0, 1, 0, 0, 1, 0, 1, 0, };
+            int n = c.Length - 1;
             int safe = 0;
             int danger = 1;
-            int jumps = 0;
+            int jumps = 1;
 
-            for (int i = 0; i <= n; i++)
+            for (int i = 1; i < n; i++)
             {
-                if (clouds[0] == safe && clouds[1] == safe)
+                if (c[i] == safe)
                 {
-                    jumps++;
-                }
-                if (clouds[i + 1] == danger)
-                {
-                    jumps++;
+                    if (c[i - 1] == danger)
+                    {
+                        jumps++;
+                        if (i < n - 1)
+                        {
+                            if (c[i + 1] == safe && c[i + 2] == safe)
+                            {
+                                jumps++;
+                            }
+                        }
+                    }
+                    if (c[i + 1] == danger)
+                    {
+                        jumps++;
+                    }
                 }
             }
             Console.WriteLine(jumps);
