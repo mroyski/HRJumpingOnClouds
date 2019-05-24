95e44b750737edaf0c61290391553e8608295642
mroyski <mroyski@gmail.com>
2019-05-24 02:21:26 -0400

    changed iteration

--- HRJumpingOnClouds/Program.cs
+++ HRJumpingOnClouds/Program.cs
@@ -10,6 +10,23 @@ namespace HRJumpingOnClouds
     {
         static void Main(string[] args)
         {
+            int[] clouds = new int[] { 0, 0, 1, 0, 0, 1, 0, };
+            int n = clouds.Length;
+            int safe = 0;
+            int danger = 1;
+            int jumps = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (clouds[i] == danger)
+                {
+                    if (clouds[i - 1] == safe)
+                    {
+                        jumps++;
+                    }
+                }
+            }
+            Console.WriteLine(jumps);
         }
     }
 }
